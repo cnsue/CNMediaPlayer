fa8b9389a9d99835d03af2791ae00afe34640df9
scn孙长宁 <cnsun@Ctrip.com>
2018-06-20 17:17:59 +0800

    创建项目

---
+++ CNMedia/CNMedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CNMedia
+//
+//  Created by scn孙长宁 on 2018/6/20.
+//  Copyright © 2018年 scn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
